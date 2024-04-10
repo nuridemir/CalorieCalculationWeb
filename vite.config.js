be896279f72599873f5084bf0347a113fc3bcf6a
Demir <ndemircv@hotmail.com>
2024-04-11 00:55:43 +0300

    added gh pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  "base": "/CalorieCalculationWeb",
   plugins: [react()],
   resolve: {
     alias: {
