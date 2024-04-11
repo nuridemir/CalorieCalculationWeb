980c1eb44950cc57b3e7b511f2efeaf6603fd7b9
Demir <ndemircv@hotmail.com>
2024-04-11 12:39:42 +0300

    uploaded react helmek

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,18 @@ import Header from "@/components/header"
 import CalcuteForm from "./components/calcute-form"
 import Faqs from "@/components/faqs"
 import Footer from "@/components/footer"
+import { Helmet } from "react-helmet"
 
 function App() {
 
   return (
     <main>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Kalori, Macro Hesaplayıcı</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+        <meta name="description" content="Mevcut kilonuzu girerek günlük almanız gereken makro değerlerini hesaplamak için tıklayın." />
+      </Helmet>
       <Header />
       <CalcuteForm />
       <Faqs />
